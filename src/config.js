d68dd36b76def30823024d82ef758ed0911aa318
Simona <monisimi99@gmail.com>
2021-03-25 22:04:50 +0200

    feat: added configs

--- src/config.js
+++ src/config.js
@@ -24,4 +24,62 @@ export default {
   assets: {
     root: '/',
   },
+  player: {
+    body: {
+      texture: 'rover',
+      x: 480,
+      y: -30,
+      angle: 0,
+
+    },
+    shadow: {
+      texture: 'rover-shadow',
+      x: 480,
+      y: 60,
+      angle: 0,
+    },
+    healthBar: {
+      texture: 'health-bar',
+      x: 460,
+      y: -100,
+    },
+  },
+  bot: {
+    body: {
+      texture: 'rover',
+      x: -670,
+      y: -70,
+      angle: 180,
+
+    },
+    shadow: {
+      texture: 'rover-shadow',
+      x: -670,
+      y: -150,
+      angle: 180,
+    },
+    healthBar: {
+      texture: 'health-bar',
+      x: -820,
+      y: -90,
+    },
+  },
+  shield: {
+    active: {
+      texture: 'shield-active',
+    },
+    inactive: {
+      texture: 'shield-inactive',
+    },
+    upperShield: {
+      x: 30,
+      y: -60,
+      angle: 310,
+    },
+    lowerShield: {
+      x: -60,
+      y: 25,
+      angle: 325,
+    }
+  }
 };
